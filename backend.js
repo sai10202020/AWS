2b0bc86180cc91cc3f1f189c602b22e3c58633dd
sai10202020 <137087748+sai10202020@users.noreply.github.com>
2025-05-28 16:38:42 +0530

    Update backend.js

--- backend.js
+++ backend.js
@@ -594,11 +594,11 @@ app.post('/api/claims/rejected', authenticateUser, async (req, res) => {
 });
 
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-    console.log(`Access frontend at http://localhost:${PORT}`);
-});
-
-// app.listen(5000, '0.0.0.0', () => {
-//   console.log('Server running at http://0.0.0.0:5000');
+// app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+//     console.log(`Access frontend at http://localhost:${PORT}`);
 // });
+
+app.listen(5000, '0.0.0.0', () => {
+  console.log('Server running at http://0.0.0.0:5000');
+});
